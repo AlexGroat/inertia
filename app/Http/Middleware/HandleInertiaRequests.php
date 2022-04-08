b7658895ec692f01594a93357c09437432abad41
Alex Groat <alex.groat.95@gmail.com>
2022-04-08 13:59:39 +0930

    shared data

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,11 +36,10 @@ class HandleInertiaRequests extends Middleware
     {
         return array_merge(parent::share($request), [
             'auth' => [
-                'user' => $request->user(),
-            ],
-            'ziggy' => function () {
-                return (new Ziggy)->toArray();
-            },
+                'user' => [
+                    'username' => 'JohnDoe'
+                ]
+            ]
         ]);
     }
 }
--- resources/js/Pages/Shared/Layout.vue
+++ resources/js/Pages/Shared/Layout.vue
@@ -1,24 +1,30 @@
 <template>
-  <section class="bg-gray-200 rounded p-3">
+  <section class="p-6 bg-gray-200">
     <header class="flex justify-between">
-      <h1 class="font-bold text-lg">My App</h1>
+      <div class="flex items-center">
+        <h1 class="font-bold text-lg">My App</h1>
+
+        <p class="text-sm ml-4">Welcome Back, {{ username }}!</p>
+      </div>
+
       <Nav />
     </header>
   </section>
-
   <section class="p-6">
     <div class="max-w-3xl mx-auto">
       <slot />
     </div>
   </section>
 </template>
-
 <script>
-import Nav from "./Nav.vue";
-
+import Nav from "./Nav";
 export default {
-  components: {
-    Nav,
+  components: { Nav },
+
+  computed: {
+    username() {
+      return this.$page.props.auth.user.username;
+    },
   },
 };
 </script>
