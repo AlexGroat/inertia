ee9019fc5b09949c79fab04569617459e96e45b6
Alex Groat <alex.groat.95@gmail.com>
2022-04-08 12:07:49 +0930

    preserve scroll position time 3:16

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,16 +1,22 @@
 <template>
   <h1>Users</h1>
 
-    <Nav />
+  <Nav />
+
+  <div style="margin-top: 1000px">
+    <p>Current time is {{ time }}.</p>
+
+    <Link href="/users" class="text-blue-500" preserveScroll> Refresh</Link>
+  </div>
 </template>
 
 <script>
-import Nav from './Shared/Nav.vue';
+import Nav from "./Shared/Nav.vue";
+import { Link } from "@inertiajs/inertia-vue3";
 
 export default {
-    components: {
-       Nav
-    }
- 
+  components: { Nav, Link },
+
+  props: { time: String },
 };
 </script>
